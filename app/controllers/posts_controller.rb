af9a724e2355c308e23e32c61036a2f570ddffbd
Trevor Jameson <github email address>
2018-09-24 09:53:47 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,18 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
 end
